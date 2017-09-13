f6437c398cdbd92991a59223c0653313bbfe2ab1
koyan <koyandianthos@gmail.com>
2017-09-13 12:45:07 +0200

    removed irrelevant delayer (since we dont mount the plugins directory any more

--- docker/app/bin/entrypoint.sh
+++ docker/app/bin/entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-until cd /var/www/html/public/wp-content/plugins ; do
-    echo "Wait for project folder mount"
-    sleep 5
-done
-
 cp /var/www/html/wp-cli.yml.default /var/www/html/wp-cli.yml
 
 su -m -c "sed -i s:db_user:$DBUSER: /var/www/html/wp-cli.yml" -ls /bin/sh nginx &&
