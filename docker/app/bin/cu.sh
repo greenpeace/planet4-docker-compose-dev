83e1a47708f3ba73b2a4b07b1cf6b78f6356045f
Kiriakos Diamantis <replicator31@hotmail.com>
2017-09-26 09:51:08 +0300

    Added composer scripts and static network for containers

---
+++ docker/app/bin/cu.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+su -m -c "cd /var/www/html && composer update" -ls /bin/sh nginx
--- docker/app/bin/entrypoint.sh
+++ docker/app/bin/entrypoint.sh
@@ -7,9 +7,10 @@ done
 
 declare -a R_ARRAY=($(echo $PLUGINS_REPOS | awk 'BEGIN{FS="__";} {for (i = 1; i <=NF; i++) print $i }'))
 for h in "${R_ARRAY[@]}"; do
-echo $h
     git clone $h
 done
+
+resize -s 30 120
 
 cp /var/www/html/wp-cli.yml.default /var/www/html/wp-cli.yml
 
@@ -17,7 +18,7 @@ su -m -c "sed -i s:db_user:$DBUSER: /var/www/html/wp-cli.yml" -ls /bin/sh nginx 
 su -m -c "sed -i s:db_pass:$DBPASS: /var/www/html/wp-cli.yml" -ls /bin/sh nginx &&
 su -m -c "sed -i s:wordpress:$DBNAME: /var/www/html/wp-cli.yml" -ls /bin/sh nginx &&
 su -m -c "sed -i s:127.0.0.1:$DBHOST: /var/www/html/wp-cli.yml" -ls /bin/sh nginx &&
-su -m -c "cd /var/www/html && composer install && composer run-script docker-site-install && composer run-script site-dev" -ls /bin/sh nginx &&
+su -m -c "cd /var/www/html && composer install && composer run-script docker-site-install" -ls /bin/sh nginx &&
 
 php-fpm &
 nginx -g "daemon off;"
